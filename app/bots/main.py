bced283749d113760bd7dddd6eb5fd1c942d9d41
Desside <desside.programming@gmail.com>
2024-03-26 18:49:59 +0200

    + Review Code

--- app/bots/main.py
+++ app/bots/main.py
@@ -2,7 +2,6 @@ import sys
 sys.path.append('/code/app/bots')
 sys.path.append('/code/app/bots/handlers')
 sys.path.append('/code/app/bots/commands')
-# sys.path.append('/code/app/bots/database')
 sys.path.append('/code/app/bots/tickets')
 
 import discord
@@ -44,12 +43,6 @@ class Client(commands.Bot):
             "tickets.ticket_cheap",
             "tickets.ticket_rep",
             "tickets.ticket_set"
-            # "cogs.ticket_system",
-            # "cogs.add_role_system",
-            # "cogs.ember_creator_system",
-            # "cogs.nickname_on_join",
-            # "cogs.tickets.ticket_cheap",
-            # "cogs.tickets.ticket_report"
             ]
     async def setup_hook(self):
         for ext in self.cogslist:
@@ -95,49 +88,6 @@ Slash Commands Next: //
 help
 """
 
-
-@Client.tree.command(name="stop", description="stop")
-async def stop_bot(interaction: discord.Interaction):
-    if interaction.user.id == 960251916762378241:
-        await interaction.response.send_message(f'Завершение бота')
-        await Client.close()
-@Client.tree.command(name="embed", description="embed")
-async def embed(interaction: discord.Interaction):
-    view = EmbedCreator(bot=Client)
-    await interaction.response.send_message(embed=view.get_default_embed, view=view)
-# @Client.tree.command(name="create-database", description="Создание таблицы в базе-данных")
-# @commands.has_permissions(administrator=True)
-# async def save_image(interaction: discord.Interaction):
-#     await interaction.response.send_message(f'Создание таблицы в базе-данных')
-#     db_manager = dbMaria.MariaDBManager(user='root', password='', host='localhost', database='lsc-bot-system-database')
-#     columns = {'id': 'INT AUTO_INCREMENT PRIMARY KEY', 'user_id': 'VARCHAR(255)', 'image': 'BLOB', 'text_message': 'TEXT'}
-#     db_manager.create_table('new_table', columns)
-
-
-# @Client.tree.command(name="execute-sql", description="admin")
-# async def command_permissions(interaction: discord.Interaction):
-#     await interaction.response.send_message(f'Даем разрешение на команду')
-#     db_manager = dbMaria.MariaDBManager(user='root', password='', host='localhost', database='lsc-bot-system-database')
-#     db_manager.execute_all_sql_files_in_subfolder('sql-data')
-
-
-# @Client.tree.command(name="save-roles", description="Сохранение ролей")
-# @commands.has_permissions(administrator=True)
-# async def save_roles(interaction: discord.Interaction):
-#     guild = interaction.guild
-#     db_manager = dbMaria.MariaDBManager(user='root', password='', host='localhost', database='lsc-bot-system-database')
-#     db_manager.delete_all_data('roles')
-#     for role in guild.roles:
-#         db_manager.insert_data('roles', data={'role_id': str(role.id), 'role_name': role.name})
-#     await interaction.response.send_message("Роли сохранены в базе данных.")
-
-
-# @Client.tree.command(name="get-guilds", description="Displays information about the bot's guilds")
-# async def get_guilds(interaction: discord.Interaction):
-#     # Check if the bot has the 'View Members' permission in the current guild
-#     if not (await commands.member_has_permissions(interaction.guild, interaction.user, view_members=True)):
-#         await interaction.response.send_message("I don't have permission to view guild members! Please grant me the 'View Members' permission.")
-#         return
 @Client.tree.command(name="test-garage", description="embed")
 async def embed(interaction: discord.Interaction):
     embed_main = discord.Embed(color=0xffffff, title="📌‧ нᴀɯи ᴦᴀᴩᴀжи!", description=f"""Если у вас возникают вопросы.
@@ -154,101 +104,6 @@ async def embed(interaction: discord.Interaction):
     await interaction.channel.send(embed=embed_main)
     await interaction.channel.send(embed=embed_info)
     await interaction.response.send_message("Ready", ephemeral=True)
-
-@Client.tree.command(name="test-reports", description="embed")
-async def embed(interaction: discord.Interaction):
-    embed_main = discord.Embed(color=0xffffff, title="🌏 ‧ 𝐋𝐒𝐂 - 𝙎𝙚𝙧𝙫𝙞𝙘𝙚𝙨", description="""
-- Если вы столкнулись с какой-либо **проблемой**
-на сервере, будь то **нарушение** *правил*, *баг*, 
-или просто нужна *помощь*, 
-``ᴄоздᴀйᴛᴇ ᴛиᴋᴇᴛ ʙ нᴀɯᴇй ᴄиᴄᴛᴇʍᴇ!``
-- **__Спасибо за ваше сотрудничество!)__**
-- <#1205649863937761370>
-
-- Если вы хотите **Создать Тикет**
- - Нажмите на кнопку *__Создать Тикет__*
-                            """)
-    embed_main.set_footer(text="**𝐋𝐒𝐂 - 𝙎𝙚𝙧𝙫𝙞𝙘𝙚𝙨**  [✅]")
-    embed_main.set_image(url="https://i.imgur.com/38rEjs8.png")
-    await interaction.channel.send(embed=embed_main)
-    await interaction.response.send_message("Ready", ephemeral=True)
-
-@Client.tree.command(name="test-rent", description="embed")
-async def embed(interaction: discord.Interaction):
-    embed_main = discord.Embed(color=0xe6ca00, title="💼 ‧ 𝐋𝐒𝐂 - 𝙎𝙚𝙧𝙫𝙞𝙘𝙚", description="""
-``ᴄоздᴀйᴛᴇ ᴛиᴋᴇᴛ ʙ нᴀɯᴇй ᴄиᴄᴛᴇʍᴇ!``
-- **__Спасибо за ваше сотрудничество!)__**
-- <#1205649899631411290>
-                            """)
-    embed_main.add_field(name="🟢‧ Выберите категорию", value="""
-- 🛻⇢ **Легковые** автомобили
-- 🚛⇢ **Грузовые** автомобили
-- 🛵⇢ **Мотоциклы**
-- 🛬⇢ **Самолеты**
-- 🚤⇢ **Корабли**(Лодки)
-                        """, inline=True)
-    embed_main.add_field(name="⏱️‧ Срок", value="""
-- *от 1 часа до 7 дней* (**__Спрашивайте отдельно__**)
-                        """, inline=False)
-    embed_main.set_footer(text="**𝐋𝐒𝐂 - 𝙎𝙚𝙧𝙫𝙞𝙘𝙚𝙨**  [✅]")
-    embed_main.set_image(url="https://i.imgur.com/QMs5e9Q.png")
-    await interaction.channel.send(embed=embed_main)
-    await interaction.response.send_message("Ready", ephemeral=True)
-
-@Client.tree.command(name="test-rep", description="embed")
-async def embed(interaction: discord.Interaction):
-    embed_main = discord.Embed(color=0xff0044, title="🔧 ‧ 𝐋𝐒𝐂 - 𝙎𝙚𝙧𝙫𝙞𝙘𝙚𝙨", description=f"""
-``ᴄоздᴀйᴛᴇ ᴛиᴋᴇᴛ ʙ нᴀɯᴇй ᴄиᴄᴛᴇʍᴇ!``
-- **__Спасибо за ваше сотрудничество!)__**
-- <#1205649937480679474>
-                            """)
-    embed_main.add_field(name="🔧‧ Наши Услуги", value="""
-- Починить ваш двигатель: 150.000$
-- Провести диагностику вашего транспорта: 1$
-( **__Только для машин с двигателем__** ) [ *Обговаривать на месте* ]
-                        """)
-    embed_main.set_image(url="https://i.imgur.com/T93kYwp.png")
-    await interaction.channel.send(embed=embed_main)
-    await interaction.response.send_message("Ready", ephemeral=True)
-    
-@Client.tree.command(name="test-set", description="embed")
-async def embed(interaction: discord.Interaction):
-    embed_main = discord.Embed(color=0x9bb8a0, title="⏰ ‧ 𝐋𝐒𝐂 - 𝙎𝙚𝙧𝙫𝙞𝙘𝙚𝙨", description="""
-``ᴄоздᴀйᴛᴇ ᴛиᴋᴇᴛ ʙ нᴀɯᴇй ᴄиᴄᴛᴇʍᴇ!``
-- **__Спасибо за ваше сотрудничество!)__**
-- <#1205649974843678801>
-                            """)
-    embed_main.add_field(name="🔧‧ Наши преимущества:", value="""
-- Минимальная наценка: 
- - **__Мы предлагаем самые выгодные цены__** 
-  - **Все настройки по 250.000$**: 
-   - __Доступны любые изменения__ 
-   - __для вашего автомобиля__.
-- **Услуга настройщика**: 
-__Наши опытные мастера__ 
-помогут вам создать неповторимый стиль.
-                        """)
-    embed_main.set_image(url="https://i.imgur.com/7amWKkn.png")
-    await interaction.channel.send(embed=embed_main)
-    await interaction.response.send_message("Ready", ephemeral=True)
-
-@Client.tree.command(name="test-feedback", description="embed")
-async def embed(interaction: discord.Interaction):
-    embed_main = discord.Embed(color=0x27ff78, title="📙 ‧ 𝐋𝐒𝐂 - 𝙎𝙚𝙧𝙫𝙞𝙘𝙚𝙨", description="""
-``ᴄоздᴀйᴛᴇ ᴛиᴋᴇᴛ ʙ нᴀɯᴇй ᴄиᴄᴛᴇʍᴇ!``
-- **__Спасибо за ваше сотрудничество!)__**
-- <#1205649830018547753>
-                            """)
-    embed_main.add_field(name="📙‧ Оставьте отзыв", value="""""")
-    embed_main.set_image(url="https://i.imgur.com/6L7xbfr.png")
-    await interaction.channel.send(embed=embed_main)
-    await interaction.response.send_message("Ready", ephemeral=True)
-
-
-
-
-
-
 
 @Client.event
 async def on_member_join(member: discord.Member):
